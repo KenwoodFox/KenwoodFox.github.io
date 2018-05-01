66fb8255e7a347dac72acdebc55870a427c5d65c
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2018-05-01 11:25:01 -0400

    Look i worked on a thing
    Im so proud of myself

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,4 @@
-<div class="sidebar"><!-- sidebar data -->
+/*<div class="sidebar"><!-- sidebar data -->
 <h3>Latest News</h3>
 <h4>No new news</h4>
 <h5>April 5th, 2018</h5>
@@ -27,3 +27,39 @@ out and adding to it as time goes on.<br>
  type="image"> </p>
 </form>
 </div>
+*/ 
+// Set up!
+var a_canvas = document.getElementById("a");
+var context = a_canvas.getContext("2d");
+
+// Draw the face
+context.fillStyle = "yellow";
+context.beginPath();
+context.arc(95, 85, 40, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+context.lineWidth = 2;
+context.stroke();
+context.fillStyle = "black";
+
+// Draw the left eye
+context.beginPath();
+context.arc(75, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the right eye
+context.beginPath();
+context.arc(114, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the mouth
+context.beginPath();
+context.arc(95, 90, 26, Math.PI, 2*Math.PI, true);
+context.closePath();
+context.fill();
+
+// Write "Hello, World!"
+context.font = "30px Garamond";
+context.fillText("Hello, World!",15,175);
